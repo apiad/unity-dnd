cf70b642a4f4bc796a1869d77fd6f706fbf28372
Alejandro Piad <alepiad@gmail.com>
2020-02-12 21:43:19 +0100

    First iteration

---
+++ Assets/FollowDice.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowDice : MonoBehaviour
+{
+    public Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = target.position + Vector3.up * 1;
+        this.transform.LookAt(target);
+    }
+}
