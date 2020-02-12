f0c2e57f27fb3719a8373ccbeda98dfaa6149b37
Alejandro Piad <alepiad@gmail.com>
2020-02-12 21:43:51 +0100

    Reformat

--- Assets/RollDice.cs
+++ Assets/RollDice.cs
@@ -22,7 +22,8 @@ public class RollDice : MonoBehaviour
         float d = Time.deltaTime;
         float t = d * this.PerSecondTorque;
 
-        if (Input.GetKeyDown(KeyCode.Space)) {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
             this.body.MovePosition(new Vector3(0, 4, 0));
             this.body.MoveRotation(Random.rotation);
             this.body.velocity = Vector3.zero;
@@ -35,7 +36,8 @@ public class RollDice : MonoBehaviour
             this.body.AddTorque(t, 0, t);
             this.force += d * this.PerSecondSpeed;
         }
-        else if (Input.GetKeyUp(KeyCode.Space)) {
+        else if (Input.GetKeyUp(KeyCode.Space))
+        {
             this.body.AddForce(this.force * new Vector3(Random.value - 0.5f, 0, Random.value - 0.5f));
             this.body.useGravity = true;
         }
